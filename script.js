c89c214f7f5bce6a8d26e7c0b9223c6a3d38c515
ADPROXHIREN <mandaliyahiren6@gmail.com>
2025-03-19 09:18:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you! Your message has been sent.");
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
