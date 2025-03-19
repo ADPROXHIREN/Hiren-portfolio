e63fc2d68c0fa6e1f1b91d3d1c2906b22deeb3fd
ADPROXHIREN <mandaliyahiren6@gmail.com>
2025-03-19 09:35:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const links = document.querySelectorAll("nav ul li a");
-    
+
     links.forEach(link => {
         link.addEventListener("click", function(event) {
             event.preventDefault();
